1219fdcd233330b4db0430f6992dac3b87a74ca0
Jonathan Osmond <jonathancosmond@gmail.com>
2018-09-21 14:13:59 -0400

    initial commit
    -placeholder imgs generated on gems
    -currently using flat array and arithmetic to orgranize gems

---
+++ Scripts/Gem.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+#should be class level, how do you make this static in GDSCRIPT?
+enum Symbols {
+	TRIANGLE,
+	SQUARE,
+	DIAMOND,
+	TRAPAZOID,
+	STAR,
+	CIRCLE
+	}
+var symbol_colors = {
+	Symbols.TRIANGLE: Color(0,1,0), #green
+	Symbols.SQUARE: Color(0,0,1), #blue
+	Symbols.DIAMOND: Color(1,0,0), #red
+	Symbols.TRAPAZOID: Color(1,0,1), #purple
+	Symbols.STAR:Color(1,1,0),
+	Symbols.CIRCLE: Color(.75,1,1)
+}
+	
+
+export (Symbols) var symbol = Symbols.TRIANGLE
+var board_index
+var size = 64
+
+func _ready():
+	if not $Sprite.texture:
+		var placeholder_img = Image.new()
+		placeholder_img.create(size,size,false, Image.FORMAT_RGBAH)
+		placeholder_img.fill(symbol_colors[symbol])
+		var placeholder_texture = ImageTexture.new()
+		
+		placeholder_texture.create_from_image(placeholder_img)
+		$Sprite.texture = placeholder_texture
